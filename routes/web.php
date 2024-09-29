8e74b6f590daa719f9695796d4a675f475163ef9
richardbilan <157667872+richardbilan@users.noreply.github.com>
2024-09-30 00:40:42 +0800

    GUDNIGHT UPDATE

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::get('/welcome', function (Request $request) {
 })->name('welcome'); //ini kapag 18 redirect sya sa mismong welcome page ta
 
 Route::get('/restricted-dashboard', function () {
-    return "Access Restricted GURANG KANA! DAE KANA MAG KASTA!"; //digdi su messge na maluwas sa 21
+    return "I MISS YOU"; //digdi su messge na maluwas sa 21
 });
 Route::get('/access-denied', function () {
     return "Access Denied PATAL MINOR KA!"; //ini man sa less than 18
